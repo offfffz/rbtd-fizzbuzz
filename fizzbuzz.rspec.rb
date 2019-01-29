5296a63305f8199b8d1eb8bd2d33fad0af301339
off <offfffz@gmail.com>
2019-01-29 17:23:04 +0700

    introducing shared_examples

--- fizzbuzz.rspec.rb
+++ fizzbuzz.rspec.rb
@@ -3,36 +3,21 @@ require './fizzbuzz'
 
 RSpec.describe FizzBuzz do
   describe '#check' do
-    let(:input) { [] }
+    shared_examples 'fizzbuzz behavior' do |input, output|
+      context "when given #{input}" do
+        it "returns #{output}" do
+          expect(described_class.check(input)).to eq(output)
+        end
 
-    subject { described_class.check(input) }
-
-    context 'when given [1]' do
-      let(:input) { [1] }
-
-      it { is_expected.to eq(['1']) }
-      it { is_expected.to be_an(Array) }
+        it 'returns array' do
+          expect(described_class.check(input)).to be_an(Array)
+        end
+      end
     end
 
-    context 'when given [3]' do
-      let(:input) { [3] }
-
-      it { is_expected.to eq(['Fizz']) }
-      it { is_expected.to be_an(Array) }
-    end
-
-    context 'when given [5]' do
-      let(:input) { [5] }
-
-      it { is_expected.to eq(['Buzz']) }
-      it { is_expected.to be_an(Array) }
-    end
-
-    context 'when given [15]' do
-      let(:input) { [15] }
-
-      it { is_expected.to eq(['FizzBuzz']) }
-      it { is_expected.to be_an(Array) }
-    end
+    include_examples 'fizzbuzz behavior', [1], ['1']
+    include_examples 'fizzbuzz behavior', [3], ['Fizz']
+    include_examples 'fizzbuzz behavior', [5], ['Buzz']
+    include_examples 'fizzbuzz behavior', [15], ['FizzBuzz']
   end
 end
