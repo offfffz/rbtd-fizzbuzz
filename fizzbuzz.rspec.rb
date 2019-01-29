ab08915acbb50d9ec02a049901ee8da97a47c181
off <offfffz@gmail.com>
2019-01-29 17:12:27 +0700

    puts spec under desccription of FizzBuzz#check

--- fizzbuzz.rspec.rb
+++ fizzbuzz.rspec.rb
@@ -2,19 +2,21 @@ require 'rspec'
 require './fizzbuzz'
 
 RSpec.describe FizzBuzz do
-  it 'returns ["1"] when given [1]' do
-    expect(FizzBuzz.check([1])).to eq(["1"])
-  end
+  describe '#check' do
+    it 'returns ["1"] when given [1]' do
+      expect(FizzBuzz.check([1])).to eq(["1"])
+    end
 
-  it 'returns ["Fizz"] when given [3]' do
-    expect(FizzBuzz.check([3])).to eq(["Fizz"])
-  end
+    it 'returns ["Fizz"] when given [3]' do
+      expect(FizzBuzz.check([3])).to eq(["Fizz"])
+    end
 
-  it 'returns ["Buzz"] when given [5]' do
-    expect(FizzBuzz.check([5])).to eq(["Buzz"])
-  end
+    it 'returns ["Buzz"] when given [5]' do
+      expect(FizzBuzz.check([5])).to eq(["Buzz"])
+    end
 
-  it 'returns ["FizzBuzz"] when given [15]' do
-    expect(FizzBuzz.check([15])).to eq(["FizzBuzz"])
+    it 'returns ["FizzBuzz"] when given [15]' do
+      expect(FizzBuzz.check([15])).to eq(["FizzBuzz"])
+    end
   end
 end
